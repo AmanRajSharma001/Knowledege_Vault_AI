d8d26deb9192b9aade586360802a9219d6d7c567
Virat Agnihotri <viratagnihotri178@gmail.com>
2026-06-20 21:51:41 +0530

    login page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,20 @@ import NavBar from "./components/NavBar"
 import Trash from "./components/trash"
 import LandingPage from "./components/LandingPage";
 import Login from "./components/Login"
-import SignIn from "./components/SignUp"
+import SignUp from "./components/SignUp";
 function App() {
   const [count, setCount] = useState(0)
   const message="hello";
   const [Active,SetActive]=useState("")
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<LandingPage />}/>
-    <Route path="/login" element={<Login />}/>
-    <Route path="signup" element={<SignIn />}/>
-   </Routes>
-   </BrowserRouter>
+    <SignUp />
+  //  <BrowserRouter>
+  //  <Routes>
+  //   <Route path="/" element={<LandingPage />}/>
+  //   <Route path="/login" element={<Login />}/>
+  //   <Route path="signup" element={<SignIn />}/>
+  //  </Routes>
+  //  </BrowserRouter>
   )
 }
 
